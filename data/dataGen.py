f7145a0bed57de9bf728d369f0848ed69c911694
Cameron <cameron.farzaneh@gmail.com>
2017-11-05 19:04:29 -0500

    ANOVA testing dataSet

--- data/dataGen.py
+++ data/dataGen.py
@@ -45,30 +45,51 @@ def addNoise(data, amp, scale):
 
     return noisyData
 
-def create_dataset():
+def create_dataset(mymean=np.array([7,-7]), myvar=np.array([20,20]), label_m=1, label_v=1, mysums=[]):
 
+    print(mymean, myvar, 'oooooooooooooooooo')
     numelems = int(1e5)
     x = np.linspace(-14,14,28)
     y = np.linspace(-14,14,28)
     X, Y = np.meshgrid(x,y)
     dataset = np.ndarray((numelems,28*28))
     labels = np.ndarray((numelems))
+    #varianceSmall = [np.array((5,5))]
+    #varianceLarge = [np.array((20,20))]
     variances = [np.array((5,5)), np.array((20,20))]
     means = [np.array((-7,7)), np.array((7,-7))]
+    #meansLeft = [np.array((-7,7))]
+    #meansRight = [np.array((7,-7))]
     label_ids = {(0,0):0, (0,1):1, (1,0):2, (1,1):3}
+
+    var_index = np.random.choice(range(len(variances)))
+    #print('$$$$$$$$$$$$$$$$$$$$$$$$$$$', var_index)
     for i in tqdm.tqdm(range(numelems)):
-        var_index = np.random.choice(range(len(variances)))
-        mean_index = np.random.choice(range(len(means)))
-        mean = means[mean_index]
-        var = variances[var_index] + np.random.normal()
-        labels[i] = label_ids[(mean_index, var_index)]
+        #var_index = np.random.choice(range(len(varianceSmall)))
+        #mean_index = np.random.choice(range(len(meansLeft)))
+        mean = mymean
+        var = myvar + np.random.normal()
+        labels[i] = label_ids[(label_m, label_v)]
         dataset[i] = gauss2D(X, Y, mean=mean, var=var).flatten()
+        #print(mean, var)
+    sum_data = np.sum(dataset)
+    mysums.append([sum_data])
+    print('******************\n', mysums)
+    
+    if label_m==1 and label_v==1:
+        create_dataset(means[0], variances[0], 0, 0, mysums)
+    elif label_m==0 and label_v==0:
+        create_dataset(means[0], variances[1], 0, 1, mysums)
+    elif label_m==0 and label_v==1:
+        create_dataset(means[1], variances[0], 1, 0, mysums)
+    else:
+        pass
+
     np.save('single_gaussians_sizes=2_locs=2', dataset)
     np.save('single_gaussians_sizes=2_locs=2_labels', labels)
 
 
 if __name__ == "__main__":
-
     create_dataset()
 
     #data = np.load('single_gaussians_sizes=2_locs=2.npy')
